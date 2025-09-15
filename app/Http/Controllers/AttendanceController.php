5ae82f9ab66738d3a48a3f2c3a7d637e6e96ccb5
Jimmy Espino <jimmyespino415@gmail.com>
2025-09-14 21:52:41 -0400

    Cambio a DB

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Carbon\Carbon;
 use App\Models\Attendance;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
 class AttendanceController extends Controller
@@ -23,11 +24,17 @@ class AttendanceController extends Controller
 
     public function store(Request $request){
         
-        Attendance::create([
+        // Attendance::create([
+        //     'student_id' => $request->student_id,
+        //     'date' => Carbon::now()->toDateString(),
+        //     'time' => Carbon::now()->format('H:i:s'),
+        // ]);
+        DB::table('attendances')->insert([
             'student_id' => $request->student_id,
             'date' => Carbon::now()->toDateString(),
             'time' => Carbon::now()->format('H:i:s'),
         ]);
+
         return response()->json(['status' => 'ok']);
     }
 }
